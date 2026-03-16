315d7833f442bf48342c023796e3be1afde7afcf
simrankaurrani262-crypto <simrankaurrani262@gmail.com>
2026-03-16 17:03:12 +0530

    Create __init__.py

---
+++ modules/core/__init__.py
@@ -0,0 +1,9 @@
+"""
+Core modules
+"""
+from . import start
+from . import profile
+from . import settings
+from .help import help_command
+
+__all__ = ['start', 'profile', 'settings', 'help_command']
